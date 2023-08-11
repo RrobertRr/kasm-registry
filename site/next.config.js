d2123f61495b4b7cc3859f5f96f3412863807f9e
RrobertRr <25011725+RrobertRr@users.noreply.github.com>
2023-08-11 12:51:51 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Roberts Registry',
+    description: 'Mein Eigenes KASM Registry',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://RrobertRr.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/RrobertRr/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
